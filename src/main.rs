1f5b13417e3aecba292fb70fadc7af933f97f023
Paco Gonzalez-Blanch <fblanch+gitcommit@gmail.com>
2021-12-15 01:45:43 +0100

    day 7 boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 use std::fs::File;
 use std::io::{BufReader, BufRead, Error};
 
+fn day_n(input_path:String) -> Result<String, Error> {
+    let input = File::open(input_path)?;
+    let buffered = BufReader::new(input);
+    
+    for line in buffered.lines() {        
+        let current:String = line?; 
+        let temp_vec:Vec<&str> = current.split(',').collect();     
+    }
+
+    let result:String = format!("result");
+    
+    Ok(result)
+}
+
+
 fn day7(input_path:String) -> Result<String, Error> {
     let input = File::open(input_path)?;
     let buffered = BufReader::new(input);
