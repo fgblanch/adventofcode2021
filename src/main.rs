6a4736b953af1b02af0b36bf9be3291038064673
Paco Gonzalez-Blanch <fblanch+gitcommit@gmail.com>
2021-12-15 21:04:03 +0100

    Day8 part A done

--- src/main.rs
+++ src/main.rs
@@ -20,12 +20,30 @@ fn day8(input_path:String) -> Result<String, Error> {
     let input = File::open(input_path)?;
     let buffered = BufReader::new(input);
     
+    let mut digit_counter = 0;
+
     for line in buffered.lines() {        
         let current:String = line?; 
-        let temp_vec:Vec<&str> = current.split(',').collect();     
+        let temp_vec:Vec<&str> = current.split('|').collect();     
+
+        let signal_patterns_string = String::from(temp_vec[0].trim());
+        let digits_string = String::from(temp_vec[1].trim());
+
+        println!("{}", signal_patterns_string);
+        println!("{}", digits_string);
+
+        let digits_temp:Vec<&str> = digits_string.split(' ').collect();     
+        
+
+        for digit in digits_temp{
+            if (digit.trim().len() == 2) | (digit.trim().len() == 3) | (digit.trim().len() == 7) | (digit.trim().len() == 4) {
+                digit_counter+=1
+            }
+        }
+
     }
 
-    let result:String = format!("result");
+    let result:String = format!("{}", digit_counter);
     
     Ok(result)
 }
