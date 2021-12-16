0467bda4ef2714a1bbb8c460f96a07b38c06d182
Paco Gonzalez-Blanch <fblanch+gitcommit@gmail.com>
2021-12-16 23:08:59 +0100

    Day 9 part B done

--- src/main.rs
+++ src/main.rs
@@ -16,22 +16,94 @@ fn day_n(input_path:String) -> Result<String, Error> {
     Ok(result)
 }
 
+#[derive(PartialEq, Debug, Clone)]
+struct Point{
+    row:u32,
+    col:u32,
+    val:u32
+}
+
+fn find_basin(heightmap:&Vec<Vec<u8>>, points_in_basin: &mut Vec<Point>, start_point:Point)-> Vec<Point>{
+    let mut result:Vec<Point> = Vec::new();
+
+    //if not in basin    
+    if !points_in_basin.contains(&start_point) && start_point.val !=9{
+        points_in_basin.push(start_point.clone());
+        result.push(start_point.clone());
+
+        // Left
+        if start_point.col > 0 {
+            let point_tmp = Point{
+                row: start_point.row,
+                col: start_point.col - 1,
+                val: heightmap[start_point.row as usize][(start_point.col - 1) as usize] as u32
+            };
+            
+            let mut basin_tmp:Vec<Point> = find_basin(heightmap,points_in_basin, point_tmp);
+            result.append(&mut basin_tmp);
+        }
+
+        // Right 
+        if start_point.col < (heightmap.get(start_point.row as usize).unwrap().len() - 1)  as u32 {
+            let point_tmp = Point{
+                row: start_point.row,
+                col: start_point.col + 1,
+                val: heightmap[start_point.row as usize][(start_point.col + 1) as usize] as u32
+            };
+
+            let mut basin_tmp:Vec<Point> = find_basin(heightmap,points_in_basin, point_tmp);
+            result.append(&mut basin_tmp);
+        }
+
+        // Up
+        if start_point.row > 0 {
+            let point_tmp = Point{
+                row: start_point.row -1,
+                col: start_point.col,
+                val: heightmap[(start_point.row -1) as usize][start_point.col as usize] as u32
+            };
+            
+            let mut basin_tmp:Vec<Point> = find_basin(heightmap,points_in_basin, point_tmp);
+            result.append(&mut basin_tmp);
+        }
+
+        // Down
+        if start_point.row < (heightmap.len() - 1)  as u32 {
+            let point_tmp = Point{
+                row: start_point.row +1,
+                col: start_point.col,
+                val: heightmap[(start_point.row +1) as usize][start_point.col as usize] as u32
+            };
+            
+            let mut basin_tmp:Vec<Point> = find_basin(heightmap,points_in_basin, point_tmp);
+            result.append(&mut basin_tmp);
+        }
+    }
+    result
+}
+
+
+
 fn day9(input_path:String) -> Result<String, Error> {
     let input = File::open(input_path)?;
     let buffered = BufReader::new(input);
 
     let mut heightmap:Vec<Vec<u8>> = Vec::new();
+    let mut points_in_basin: Vec<Point> = Vec::new();
+    let mut low_points:Vec<Point> = Vec::new();
+    let mut basins:Vec<Vec<Point>> = Vec::new();
+
     let mut sum_low_points:u32 = 0;
     
     for line in buffered.lines() {        
         let current:String = line?; 
-        let mut row:Vec<u8> = Vec::new();
+        let mut row:Vec<u8> = Vec::new();        
 
         for a in current.chars(){
             print!("{}", a);
-            row.push(String::from(a).parse().unwrap());
+            row.push(String::from(a).parse().unwrap());            
         }
-        heightmap.push(row);
+        heightmap.push(row);        
         print!("\n");
     }    
 
@@ -72,12 +144,32 @@ fn day9(input_path:String) -> Result<String, Error> {
             if is_lowest {
                 println!("low point row:{} col:{} val:{}", i, j, current_val);
                 sum_low_points += (current_val as u32) + 1;
+                low_points.push(Point{
+                    row:i as u32,
+                    col:j as u32,
+                    val: current_val as u32
+                })
             }
         }
         
     }
 
-    let result:String = format!("{}", sum_low_points);
+    println!("{:?}", low_points);
+
+    for point in low_points{
+        let basin = find_basin(&heightmap, &mut points_in_basin, point);
+        basins.push(basin);
+    }
+
+    let mut basin_sizes:Vec<u32> = Vec::new();
+    for basin in &basins{
+        basin_sizes.push(basin.len() as u32);
+        println!("basin size:{}",basin.len())
+    }
+
+    basin_sizes.sort();
+    
+    let result:String = format!("{:?}", basin_sizes);
     
     Ok(result)
 }
