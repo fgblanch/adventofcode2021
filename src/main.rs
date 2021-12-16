f9cd2cc0e63cab671a567db353da4d973b224ed7
Paco Gonzalez-Blanch <fblanch+gitcommit@gmail.com>
2021-12-16 03:06:26 +0100

    Day8 part B almost done. Fails on segment 6

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::fs::File;
 use std::io::{BufReader, BufRead, Error};
+use std::collections::HashMap;
+use std::iter::FromIterator;
 
 fn day_n(input_path:String) -> Result<String, Error> {
     let input = File::open(input_path)?;
@@ -15,15 +17,33 @@ fn day_n(input_path:String) -> Result<String, Error> {
     Ok(result)
 }
 
+fn sort_segments(word:&String) -> String{    
+    let s_slice: &str = word;
+    let mut chars: Vec<char> = s_slice.chars().collect();
+    chars.sort();
+    let s:String = String::from_iter(chars);
+    s
+}
 
 fn day8(input_path:String) -> Result<String, Error> {
     let input = File::open(input_path)?;
     let buffered = BufReader::new(input);
     
-    let mut digit_counter = 0;
+    let mut sum_output = 0;
+    
 
-    for line in buffered.lines() {        
-        let current:String = line?; 
+    // variables to capture the known decodings.
+    let mut two:String = String::new();  // one
+    let mut four:String = String::new(); // four
+    let mut three:String = String::new();
+    let mut seven:String = String::new();
+
+    for line in buffered.lines() { 
+        let current:String = line?;
+        
+        let mut encoder: HashMap<String, String> = HashMap::new();
+        let mut decoder: HashMap<String, String> = HashMap::new();
+
         let temp_vec:Vec<&str> = current.split('|').collect();     
 
         let signal_patterns_string = String::from(temp_vec[0].trim());
@@ -32,25 +52,97 @@ fn day8(input_path:String) -> Result<String, Error> {
         println!("{}", signal_patterns_string);
         println!("{}", digits_string);
 
-        let digits_temp:Vec<&str> = digits_string.split(' ').collect();     
+        let signals_temp:Vec<&str> = signal_patterns_string.split(' ').collect();     
         
-
-        for digit in digits_temp{
-            if (digit.trim().len() == 2) | (digit.trim().len() == 3) | (digit.trim().len() == 7) | (digit.trim().len() == 4) {
-                digit_counter+=1
+        for signal in &signals_temp{
+            if signal.trim().len() == 2 {
+                two = String::from(signal.trim());
+            }
+            if signal.trim().len() == 3 {
+                three = String::from(signal.trim());
+            }
+            if signal.trim().len() == 7{
+                seven = String::from(signal.trim());
+            }
+            if signal.trim().len() == 4 {
+                four = String::from(signal.trim());
             }
         }
 
+        encoder.insert(String::from("1"),two.clone());
+        encoder.insert(String::from("4"),four.clone());
+        encoder.insert(String::from("7"),three.clone());
+        encoder.insert(String::from("8"),seven.clone());
+
+        for signal in &signals_temp{
+            if signal.trim().len() == 6 {
+                println!("Two: {} Six candidate: {}",two,signal.trim());
+                // this can be either number 0,6 or 9
+                if signal.contains(two.chars().nth(0).unwrap()){
+                    // 0 or 9
+                    let mut has_all_segments = true;
+                    for segment in four.chars(){
+                        has_all_segments = has_all_segments &  signal.contains(segment);
+                    }
+                     
+                    if has_all_segments{
+                        // 9 = signal
+                        encoder.insert(String::from("9"),String::from(signal.trim()));
+                    }else{
+                        // 0 = signal              
+                        encoder.insert(String::from("0"),String::from(signal.trim()));
+                    }
+                }else{
+                    // 6 = signal
+                    encoder.insert(String::from("6"),String::from(signal.trim()));
+                }
+            }
+            if signal.trim().len() == 5 {
+                // this can be either number 2,3 or 5
+                if signal.contains(two.chars().nth(1).unwrap()){
+                    if signal.contains(two.chars().nth(0).unwrap()){
+                        // 3 = signal
+                        encoder.insert(String::from("3"),String::from(signal.trim()));
+                    }else{
+                        // 5 = signal
+                        encoder.insert(String::from("5"),String::from(signal.trim()));
+                    }
+                }else{
+                    // 2 = signal
+                    encoder.insert(String::from("2"),String::from(signal.trim()));
+                }
+            }            
+        }
+
+        println!("#2:{} #3:{} #4:{} #7:{}", two,three,four,seven);
+        println!("{:?}", encoder);        
+
+        for key in encoder.keys(){
+            decoder.insert(sort_segments(&String::from(encoder.get(key).unwrap())), String::from(key));
+        }
+        println!("{:?}", decoder);     
+        let digits_temp:Vec<&str> = digits_string.split(' ').collect();     
+        let mut value_string:String = String::from("");
+
+        for digit in &digits_temp{
+            let str_to_decode:String = sort_segments(&String::from(digit.trim()));            
+            let str_decoded:String = String::from(decoder.get(&str_to_decode).unwrap());//(sort_segments(decoder.get(digit.trim()).unwrap())).chars().nth(0).unwrap();
+            println!("{} decodes to: {}", str_to_decode, str_decoded);     
+            value_string+=&str_decoded;
+        }
+        let value_int:u32 = value_string.parse().unwrap();
+        println!("{}", value_int); 
+        sum_output +=value_int;
     }
 
-    let result:String = format!("{}", digit_counter);
+    let result:String = format!("{}", sum_output);
     
     Ok(result)
 }
 
 
 fn main() -> Result<(), Error> {
-    let result:String = day8("./input/day8.txt".to_string()).unwrap();
+    let result:String = day8("./test/day8.txt".to_string()).unwrap();
     println!("{}", result);
 
     Ok(())
