a96e32a785461479b5231f2e7ddd7db78aa9e8e6
Paco Gonzalez-Blanch <fblanch+gitcommit@gmail.com>
2021-12-17 03:18:40 +0100

    Day 11 Done PArt A & B

--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,81 @@ fn day_n(input_path:String) -> Result<String, Error> {
     Ok(result)
 }
 
+fn print_octo(octopuses: &[[u16; 10]; 10]){
+    for j in 0..10{
+        for k in 0..10{ 
+            print!("{}\t",octopuses[j][k]);
+        }
+        print!("\n");
+    }
+}
+
+fn flash(j:usize,k:usize, octopuses_temp:&mut  [[u16; 10]; 10], flash_counter: &mut u32){
+     //flash                    
+     *flash_counter+=1;
+
+     if (k>0) & (j>0){
+         octopuses_temp[j-1][k-1] += 1;
+         if octopuses_temp[j-1][k-1] == 10{
+            flash(j-1,k-1, octopuses_temp, flash_counter);
+         }
+     }
+     if j>0{
+         octopuses_temp[j-1][k] += 1;
+         if octopuses_temp[j-1][k] == 10{
+            flash(j-1,k, octopuses_temp, flash_counter);
+         }
+     }
+     if (k<9) & (j>0){
+         octopuses_temp[j-1][k+1] += 1;
+         if octopuses_temp[j-1][k+1] == 10{
+            flash(j-1,k+1, octopuses_temp, flash_counter);
+         }
+     }
+
+     if k>0{
+         octopuses_temp[j][k-1] += 1;
+         if octopuses_temp[j][k-1] == 10{
+            flash(j,k-1, octopuses_temp, flash_counter);
+         }
+     }
+     if k<9{
+         octopuses_temp[j][k+1] += 1;
+         if octopuses_temp[j][k+1] == 10{
+            flash(j,k+1, octopuses_temp, flash_counter);
+         }
+     }
+     
+     if (k>0) & (j<9){
+         octopuses_temp[j+1][k-1] += 1;
+         if octopuses_temp[j+1][k-1] == 10{
+            flash(j+1,k-1, octopuses_temp, flash_counter);
+         }
+     }
+     if j<9{
+         octopuses_temp[j+1][k] += 1;
+         if octopuses_temp[j+1][k] == 10{
+            flash(j+1,k, octopuses_temp, flash_counter);
+         }
+     }
+     if (k<9) & (j<9){
+         octopuses_temp[j+1][k+1] += 1;
+         if octopuses_temp[j+1][k+1] == 10{
+            flash(j+1,k+1, octopuses_temp, flash_counter);
+         }
+     }            
+
+}
+
+
 fn day11(input_path:String) -> Result<String, Error> {
     let input = File::open(input_path)?;
     let buffered = BufReader::new(input);
     
-    let mut octopuses: [[u8; 10]; 10] = [[0;10];10];
 
-    let mut line_counter:u8 = 0;
+    let mut octopuses: [[u16; 10]; 10] = [[0;10];10];
+
+    let mut line_counter:u16 = 0;
     for line in buffered.lines() {        
         let current:String = line?; 
         for (i,elem) in current.chars().enumerate(){
@@ -31,45 +99,64 @@ fn day11(input_path:String) -> Result<String, Error> {
         line_counter+=1;
     }
 
-    for _i in 0..100{ // steps
+    println!("\n Step: {}",0);
+    print_octo(&octopuses);
 
-        let mut flashed:bool = false;
-        let mut octopuses_temp: [[u8; 10]; 10] = [[0;10];10];
+    let mut flash_counter:u32 = 0;
+    let mut first_sync:u16 = 0;
+
+    for i in 0..500{ // steps
+
+        let mut octopuses_temp: [[u16; 10]; 10] = [[0;10];10];
+
+        for j in 0..10{
+            for k in 0..10{                
+                octopuses_temp[j][k] = octopuses_temp[j][k] + octopuses[j][k] +1;
+                //println!("j:{} k:{}",j,k);
+                if octopuses_temp[j][k] > 9{
+                    flash(j,k, &mut octopuses_temp, &mut flash_counter);                           
+                }                
+                //println!("");
+                //print_octo(&octopuses_temp);
+            } 
+        }
+        
+        let mut all_in_sync:bool = true;
+        
+        let mut last_number = octopuses_temp[0][0];
 
         for j in 0..10{
             for k in 0..10{
-                if oct1
-                octopuses_temp[j][k] = octopuses[j][k] +1
+                if octopuses_temp[j][k] > 9{
+                    octopuses_temp[j][k] = 0                
+                }
 
+                all_in_sync = all_in_sync & (last_number == octopuses_temp[j][k]);
+                last_number = octopuses_temp[j][k];
+            }
+        }
+        
+        println!("\n Step: {}",i+1);
+        print_octo(&octopuses_temp);
 
-            } 
+        octopuses = octopuses_temp;
+        
+        if all_in_sync & (first_sync==0){
+            first_sync = i;
+            break
         }
 
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    println!("{:?}",octopuses);
-
-    let result:String = format!("result");
-    
-
-
-
-
+   
+    let result:String = format!("flash counter:{} first sync: {}",flash_counter, first_sync);
 
     Ok(result)
 }
 
 
 fn main() -> Result<(), Error> {
-    let result:String = day11("./test/day11.txt".to_string()).unwrap();
+    let result:String = day11("./input/day11.txt".to_string()).unwrap();
     println!("{}", result);
 
     Ok(())
