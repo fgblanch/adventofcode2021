c2bf2d033a16d4bed88276437c402f909be96002
fgblanch <fblanch@gmail.com>
2021-12-20 16:34:55 +0100

    Refactoring for partB with dynamic programming

--- src/main.rs
+++ src/main.rs
@@ -46,45 +46,62 @@ fn day14(input_path:String) -> Result<String, Error> {
     println!("input:{} expansions:{:?}",input_comp, expansions);
 
     let mut window:String = String::from("");
-
-    for _i in 0..40{
-        //println!("\n Input: {}",input_comp);
-        window = String::from("");
-        let mut new_input:String = String::from("");
-        for c in input_comp.chars(){
-            if window.len() < 2{
-                window.push(c);
-            }else{
-                window = window.chars().nth(1).unwrap().to_string();
-                window.push(c)
-            }
-            if window.len() == 2{
-                let new_polymer:&String = expansions.get(&window).unwrap();                
-                let mut tmp_str:String = format!("{}{}",window.chars().nth(0).unwrap().to_string(),new_polymer);                
-                new_input.push_str(&tmp_str);                
-            }
-
-        }
-        new_input.push_str(&window.chars().nth(1).unwrap().to_string());
-        //println!("{}",new_input);
-        input_comp= new_input;
-
-    }
-
     let mut occurences:HashMap<String,u32> = HashMap::new();
 
     for c in input_comp.chars(){
-        let counter = occurences.entry(String::from(c)).or_default();
-        *counter+=1;
+        if window.len() < 2{
+            window.push(c);
+        }else{
+            window = window.chars().nth(1).unwrap().to_string();
+            window.push(c)
+        }
+        if window.len() == 2{
+            let counter = occurences.entry(window.to_string()).or_default();
+            *counter+=1;
+        }
     }
 
-    let result:String = format!("{:?}",occurences);
+    println!("{:?}",occurences);
+
+    for i in 0..10{
+        println!("Step: {}", i);
+        let mut new_occurences:HashMap<String,u32> = HashMap::new();
+        for key in occurences.keys(){
+            
+            let new_polymer:&String = expansions.get(key).unwrap();                
+            let new_key_a:String = format!("{}{}",key.chars().nth(0).unwrap().to_string(),new_polymer);
+            let new_key_b:String = format!("{}{}",new_polymer,key.chars().nth(1).unwrap().to_string(),);
+
+            let counter = new_occurences.entry(new_key_a.to_string()).or_default();
+            *counter+=1;
+
+            let counter = new_occurences.entry(new_key_b.to_string()).or_default();
+            *counter+=1;
+            
+                           
+        }
+        occurences = new_occurences;
+        println!("{:?}",occurences);
+    }
+
+    println!("{:?}",occurences);
+
+    let mut occurences_chars:HashMap<String,u32> = HashMap::new();
+    for key in occurences.keys(){
+        for c in key.chars(){
+            let counter = occurences_chars.entry(String::from(c)).or_default();
+            *counter+=occurences.get(key).unwrap();
+        }
+    }   
+    
+    
+    let result:String = format!("{:?}",occurences_chars);
     
     Ok(result)
 }
 
 fn main() -> Result<(), Error> {
-    let result:String = day14("./input/day14.txt".to_string()).unwrap();
+    let result:String = day14("./test/day14.txt".to_string()).unwrap();
     println!("result: {}", result);
 
     Ok(())
