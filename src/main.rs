8a3a41772c315d28c00efcce681ba670e2355fc5
Paco Gonzalez-Blanch <fblanch@gmail.com>
2021-12-22 15:15:42 +0100

    Day 14 advances

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ fn day14(input_path:String) -> Result<String, Error> {
 
     for line in buffered.lines() {        
         let current:String = line?; 
-        //let temp_vec:Vec<&str> = current.split(',').collect();     
+        
         if first_line{
             if current == "" {
                 first_line = false;
@@ -47,6 +47,7 @@ fn day14(input_path:String) -> Result<String, Error> {
 
     let mut window:String = String::from("");
     let mut occurences:HashMap<String,u32> = HashMap::new();
+    let mut occurences_chars:HashMap<String,u32> = HashMap::new();
 
     for c in input_comp.chars(){
         if window.len() < 2{
@@ -59,42 +60,45 @@ fn day14(input_path:String) -> Result<String, Error> {
             let counter = occurences.entry(window.to_string()).or_default();
             *counter+=1;
         }
-    }
 
+        let counter = occurences_chars.entry(String::from(c)).or_default();
+        *counter+=1;
+
+
+    }
     println!("{:?}",occurences);
+    println!("{:?}",occurences_chars);
 
     for i in 0..10{
         println!("Step: {}", i);
         let mut new_occurences:HashMap<String,u32> = HashMap::new();
+
         for key in occurences.keys(){
             
             let new_polymer:&String = expansions.get(key).unwrap();                
             let new_key_a:String = format!("{}{}",key.chars().nth(0).unwrap().to_string(),new_polymer);
             let new_key_b:String = format!("{}{}",new_polymer,key.chars().nth(1).unwrap().to_string(),);
 
-            let counter = new_occurences.entry(new_key_a.to_string()).or_default();
-            *counter+=1;
+            let counter_a = new_occurences.entry(new_key_a.to_string()).or_default();
+            *counter_a+=1;
 
-            let counter = new_occurences.entry(new_key_b.to_string()).or_default();
-            *counter+=1;
+            let counter_b = new_occurences.entry(new_key_b.to_string()).or_default();
+            *counter_b+=1;        
             
+            let counter_c = occurences_chars.entry(new_polymer.to_string()).or_default();
+            *counter_c+=occurences.get(key).unwrap();
                            
         }
+
         occurences = new_occurences;
-        println!("{:?}",occurences);
+         println!("{:?}",occurences);
+        println!("{:?}",occurences_chars);
     }
 
     println!("{:?}",occurences);
-
-    let mut occurences_chars:HashMap<String,u32> = HashMap::new();
-    for key in occurences.keys(){
-        for c in key.chars(){
-            let counter = occurences_chars.entry(String::from(c)).or_default();
-            *counter+=occurences.get(key).unwrap();
-        }
-    }   
+    println!("{:?}",occurences_chars);
     
-    
+        
     let result:String = format!("{:?}",occurences_chars);
     
     Ok(result)
